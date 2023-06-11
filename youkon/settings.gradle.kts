93075f66fbecc868c9e5ac9337f4c2e21a6b8aae
radcli14 <radcli14@gmail.com>
2023-06-11 13:13:43 -0400

    added the template files

---
+++ youkon/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "YouKon"
+include(":YouKon")
+include(":shared")
