a398ab4024126610cfb2edf3230bc0ef3bc9e25c
radcli14 <radcli14@gmail.com>
2023-06-13 22:15:33 -0400

    initial compose view with a background image

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/MainActivity.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/MainActivity.kt
@@ -3,11 +3,16 @@ package com.dcsim.youkon.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dcsim.youkon.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    //GreetingView(Greeting().greet())
+                    Image(
+                        painter = painterResource(id = R.drawable.background),
+                        contentDescription = "Background image of mountains",
+                        //modifier = Modifier
+                            //.padding(start = 260.dp)
+                            //.height(184.dp)
+                    )
                 }
             }
         }
