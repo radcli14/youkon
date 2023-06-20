6443ccdc538b632e4014f22c4bd502b8c28a4a47
radcli14 <radcli14@gmail.com>
2023-06-19 22:36:46 -0400

    updated scaling of background image and added text in front of it

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/MainActivity.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,29 +30,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    //GreetingView(Greeting().greet())
-                    Image(
-                        painter = painterResource(id = R.drawable.background),
-                        contentDescription = "Background image of mountains",
-                        //modifier = Modifier
-                            //.padding(start = 260.dp)
-                            //.height(184.dp)
-                    )
+                    BackgroundBox {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.icon),
+                                contentDescription = "App icon",
+                                modifier = Modifier.width(90.dp)
+                            )
+                            Text("YouKon", style = MaterialTheme.typography.h1)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun BackgroundBox(content: @Composable () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = "Background image of mountains",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        content()
+    }
 }
+
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        BackgroundBox() {
+            Text("YouKon")
+        }
     }
 }
