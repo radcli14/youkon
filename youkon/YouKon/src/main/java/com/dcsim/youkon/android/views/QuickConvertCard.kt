b2d988af63288c73635c82c8633ef66c9019e79c
radcli14 <radcli14@gmail.com>
2023-06-25 10:39:30 -0400

    moved dropdowns into a row

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/QuickConvertCard.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/QuickConvertCard.kt
@@ -1,21 +1,17 @@
 package com.dcsim.youkon.android.views
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
 import androidx.compose.material.Card
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,7 +22,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import com.dcsim.youkon.Measurement
 import com.dcsim.youkon.testMeasurement
 
 @Composable
@@ -39,7 +34,8 @@ fun QuickConvertCard() {
             .requiredWidth(360.dp)
     ) {
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             // The label at the top of the card
             Text("Quick Convert",
@@ -47,25 +43,31 @@ fun QuickConvertCard() {
                 style = MaterialTheme.typography.h6
             )
 
-            // The field that takes the user input on the numeric value of the measurement
-            TextField(
-                value = text,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                singleLine = true,
-                onValueChange = { newText ->
-                    if (newText.text.toDoubleOrNull() != null) {
-                        text = newText
-                        measurement.value = newText.text.toDouble()
-                    } else if (newText.text == "") {
-                        text = newText
-                        measurement.value = 0.0
-                    }
-                }
-            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // The field that takes the user input on the numeric value of the measurement
+                TextField(
+                    value = text,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    singleLine = true,
+                    onValueChange = { newText ->
+                        if (newText.text.toDoubleOrNull() != null) {
+                            text = newText
+                            measurement.value = newText.text.toDouble()
+                        } else if (newText.text == "") {
+                            text = newText
+                            measurement.value = 0.0
+                        }
+                    },
+                    modifier = Modifier.width(96.dp)
+                )
 
-            // Selection for which type of unit to convert from
-            FromDropdown(measurement = measurement)
-            ToDropdown(measurement = measurement)
+                // Selection for which type of unit to convert from
+                FromDropdown(measurement = measurement)
+                ToDropdown(measurement = measurement)
+            }
 
             // The display of the measurement after conversion
             Text(measurement.convertTo("feet").toString())
