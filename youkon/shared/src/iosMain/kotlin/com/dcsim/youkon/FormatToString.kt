370eabc77ea5f5de26618e5d81503449ca969490
radcli14 <radcli14@gmail.com>
2023-08-16 21:48:29 -0400

    commented out FormatSwiftString until I figure out how to link the swift script into KMM

--- youkon/shared/src/iosMain/kotlin/com/dcsim/youkon/FormatToString.kt
+++ youkon/shared/src/iosMain/kotlin/com/dcsim/youkon/FormatToString.kt
@@ -1,5 +1,5 @@
 package com.dcsim.youkon
 
 actual fun formatToString(delimiters: String, number: Double): String {
-    return FormatSwiftString(delimiters, number)
+    return number.toString()  // FormatSwiftString(delimiters, number)
 }
