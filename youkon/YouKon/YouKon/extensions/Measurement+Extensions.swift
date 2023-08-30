dcd93ef6429f764a968ee31d302c4fa2bae36a9e
radcli14 <radcli14@gmail.com>
2023-08-29 22:21:58 -0400

    temporarily building extensions to support string formatting, until I work out how to do it in kotlin multiplatform

---
+++ youkon/YouKon/YouKon/extensions/Measurement+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Measurement+Extensions.swift
+//  YouKon
+//
+//  Created by Eliott Radcliffe on 8/29/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+
+extension shared.Measurement {
+    func toSwiftString() -> String {
+        return "\(niceNumber(self.value)) \(self.unit.shortUnit)"
+    }
+    
+    func toSwiftString(in targetUnit: MeasurementUnit) -> String {
+        return self.toSwiftString() + " = " + self.convertTo(targetUnit: targetUnit).toSwiftString()
+    }
+}
