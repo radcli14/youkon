90e9b1db4005021ba6a1b4c949c1e45d821b0640
radcli14 <radcli14@gmail.com>
2023-09-22 17:47:34 -0400

    loads the default user from the json file

--- youkon/YouKon/YouKon/controller/ContentViewController.swift
+++ youkon/YouKon/YouKon/controller/ContentViewController.swift
@@ -9,17 +9,29 @@
 import Foundation
 import shared
 
+/// The `ContentViewController` is instantiated inside the primary `ContentView`, and provides the `YkUser` data to lower level controllers. Also, if a call is received to toggle editing, this will trigger opening the bottom sheet.
 class ContentViewController: ObservableObject {
     @Published var isEditingProject = false
     var project: YkProject? = nil
-    var user: YkUser
+    var user = YkUser()
     
     init() {
-        user = YkUser()
-        user.setAsTestUser()
-        print(user.asJsonString())
+        user = defaultUser
     }
     
+    /// The default user for someone opening the app for the first time is stored in `resources/defaultuser.json`
+    var defaultUser: YkUser {
+        if let path = Bundle.main.path(forResource: "defaultuser", ofType: "json"),
+            let contents = try? String(contentsOfFile: path),
+            let defaultUser = user.fromJsonString(jsonString: contents) {
+            return defaultUser
+        } else {
+            print("Failed to load the default YkUser, falling back to an empty YkUser()")
+            return YkUser()
+        }
+    }
+    
+    /// The user tapped the measuments in a project's disclosure group, toggle editable measurements sheet
     func toggleEdit(to project: YkProject) {
         isEditingProject.toggle()
         self.project = isEditingProject ? project : nil
