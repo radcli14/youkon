71f4408f30d9725421cfed0d16017e8bd1cd2de2
radcli14 <radcli14@gmail.com>
2023-09-30 20:18:52 -0400

    created the toBase and fromBase parameters

--- youkon/shared/src/commonMain/kotlin/com/dcsim/youkon/Unit.kt
+++ youkon/shared/src/commonMain/kotlin/com/dcsim/youkon/Unit.kt
@@ -1,12 +1,14 @@
 package com.dcsim.youkon
 
-enum class YkUnit {
-    KILOGRAMS, POUNDS, SLUGS,
-    METERS, FEET, INCHES,
-    NEWTONS, POUND_FORCE,
-    WATTS, HORSEPOWER,
-    JOULES, BTU,
-    PASCALS, PSI, ATM, BARS;
+enum class YkUnit(val toBase: Double) {
+    KILOGRAMS(1.0), POUNDS(0.453592), SLUGS(14.5939),
+    METERS(1.0), FEET(0.3048), INCHES(0.3048),
+    NEWTONS(1.0), POUND_FORCE(4.44822),
+    WATTS(1.0), HORSEPOWER(745.7),
+    JOULES(1.0), BTU(1055.06),
+    PASCALS(1.0), PSI(6894.76), ATM(101325.0), BARS(100000.0);
+
+    val fromBase: Double get() = 1.0 / toBase
 
     val shortUnit: String
         get() = when(this) {
@@ -45,38 +47,6 @@ enum class YkUnit {
 
     /// Conversion factor from the current measurement unit into a different unit
     fun conversionFactor(targetUnit: YkUnit): Double {
-        return when (this to targetUnit) {
-            KILOGRAMS to POUNDS -> 2.20462
-            KILOGRAMS to SLUGS -> 0.0685218
-            POUNDS to KILOGRAMS -> 1.0 / 2.20462
-            POUNDS to SLUGS -> 0.0310809
-            SLUGS to KILOGRAMS -> 14.5939
-            SLUGS to POUNDS -> 32.174
-            METERS to FEET -> 3.28084
-            METERS to INCHES -> 39.3701
-            FEET to METERS -> 0.3048
-            FEET to INCHES -> 12.0
-            INCHES to METERS -> 0.0254
-            INCHES to FEET -> 0.0833333
-            NEWTONS to POUND_FORCE -> 0.224808943
-            POUND_FORCE to NEWTONS -> 4.4482216153
-            WATTS to HORSEPOWER -> 0.00134102
-            HORSEPOWER to WATTS -> 745.7
-            JOULES to BTU -> 0.00094781712
-            BTU to JOULES -> 1055.06
-            PASCALS to PSI -> 0.00014503773773375
-            PASCALS to ATM -> 9.8692e-6
-            PASCALS to BARS -> 1e-5
-            PSI to PASCALS -> 6894.76
-            PSI to ATM -> 0.068046
-            PSI to BARS -> 0.0689476
-            ATM to PASCALS -> 101325.0
-            ATM to PSI -> 14.696
-            ATM to BARS -> 1.01325
-            BARS to PASCALS -> 100000.0
-            BARS to PSI -> 14.5038
-            BARS to ATM -> 0.986923
-            else -> 1.0
-        }
+        return toBase * targetUnit.fromBase
     }
 }
