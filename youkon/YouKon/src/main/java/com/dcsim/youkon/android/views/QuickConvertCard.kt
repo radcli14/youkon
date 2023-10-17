222195fd14d517e1ca7e86a735f9dc1a87f6a64d
radcli14 <radcli14@gmail.com>
2023-10-16 23:06:17 -0400

    organized quick convert card to be more closely aligned to the iOS architecture

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/QuickConvertCard.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/QuickConvertCard.kt
@@ -6,21 +6,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.dcsim.youkon.android.viewmodels.QuickConvertCardViewModel
 
 class QuickConvertCard {
-    val vm = QuickConvertCardViewModel()
+    private val vm = QuickConvertCardViewModel()
 
     @Composable
     fun Body() {
-        Card(
+        Surface(
+            color = Color.Gray.copy(alpha = 0.2f),
             modifier = Modifier
                 .requiredWidth(360.dp)
         ) {
@@ -36,7 +38,7 @@ class QuickConvertCard {
 
     /// The label at the top of the card
     @Composable
-    fun CardLabel() {
+    private fun CardLabel() {
         Text("Quick Convert",
             fontWeight = FontWeight.SemiBold,
             style = MaterialTheme.typography.h6
@@ -44,30 +46,49 @@ class QuickConvertCard {
     }
 
     @Composable
-    fun ContentGrid() {
+    private fun ContentGrid() {
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.fillMaxWidth()
         ) {
-
-            // Selection for which type of unit to convert from
-            FromDropdown(measurement = vm.measurement) { unit ->
-                vm.updateUnit(unit)
-            }
-            ToDropdown(equivalentUnits = vm.equivalentUnits, targetUnit = vm.targetUnit)
+            FromDromdown()
+            ToDropdown()
         }
 
         Row {
-            // The field that takes the user input on the numeric value of the measurement
-            MeasurementTextField(measurement = vm.measurement) {
-                vm.convertedText = vm.measurement.valueAndConversion(vm.targetUnit)
-            }
-
-            // The display of the measurement after conversion
-            Text(vm.convertedText,
-                style = MaterialTheme.typography.subtitle1,
-                modifier = Modifier.padding(top = 8.dp, start = 16.dp)
-            )
+            TextField()
+            ConvertedText()
         }
+    }
+
+    /// Selection for which type of unit to convert from
+    @Composable
+    private fun FromDromdown() {
+        FromDropdown(measurement = vm.measurement) { unit ->
+            vm.updateUnit(unit)
+        }
+    }
+
+    /// Selection for which type of unit to convert to
+    @Composable
+    private fun ToDropdown() {
+        ToDropdown(equivalentUnits = vm.equivalentUnits, targetUnit = vm.targetUnit)
+    }
+
+    /// The field that takes the user input on the numeric value of the measurement
+    @Composable
+    private fun TextField() {
+        MeasurementTextField(measurement = vm.measurement) {
+            vm.setConvertedText()
+        }
+    }
+
+    /// The display of the measurement after conversion
+    @Composable
+    private fun ConvertedText() {
+        Text(vm.convertedText,
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier.padding(top = 8.dp, start = 16.dp)
+        )
     }
 }
