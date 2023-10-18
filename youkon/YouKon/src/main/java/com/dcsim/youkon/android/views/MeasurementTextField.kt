3bc2ff99aaba0ea3d7fb9c96bab88c7bd86136ae
radcli14 <radcli14@gmail.com>
2023-10-17 23:06:34 -0400

    removed the outline and right aligned

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/MeasurementTextField.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/MeasurementTextField.kt
@@ -1,17 +1,18 @@
 package com.dcsim.youkon.android.views
 
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
 import com.dcsim.youkon.YkMeasurement
 
 @Composable
@@ -32,6 +33,20 @@ fun MeasurementTextField(measurement: YkMeasurement, updateMeasurement: () -> Un
             }
             updateMeasurement()
         },
-        modifier = Modifier.width(96.dp)
+        textStyle = MaterialTheme.typography.h6.copy(
+            textAlign = TextAlign.End
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            cursorColor = MaterialTheme.colors.primary,
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent,
+            backgroundColor = MaterialTheme.colors.surface.copy(alpha=0.5f)
+        ),
+        //modifier = Modifier
+            /*.background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(Color.White, Color.White)
+                )
+            )*/
     )
 }
