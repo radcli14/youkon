ebd8f8e5b1a27464b8a0a9422e3924ecbb19e037
radcli14 <radcli14@gmail.com>
2023-11-07 21:39:28 -0500

    Split out into smaller composables, similar to the swiftui version

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/ProjectsCard.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/ProjectsCard.kt
@@ -1,6 +1,8 @@
 package com.dcsim.youkon.android.views
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,13 +23,7 @@ class ProjectsCard(
     fun Body() {
         Card(modifier = Modifier.padding(16.dp)) {
             Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = "Projects",
-                    style = MaterialTheme.typography.h6,
-                    fontWeight = FontWeight.SemiBold,
-                    //modifier = Modifier.padding(bottom = 16.dp)
-                )
-
+                LabelStack()
                 LazyColumn {
                     items(vm.projects.value) { project ->
                         ProjectView(project = project)
@@ -36,25 +32,88 @@ class ProjectsCard(
             }
         }
     }
-}
-/*
-@Composable
-fun ProjectsCard(projects: List<YkProject>) {
-    Card(modifier = Modifier.padding(16.dp)) {
-        Column(modifier = Modifier.padding(16.dp)) {
+
+    @Composable
+    fun LabelStack() {
+        Row {  //(spacing: 8) {
             Text(
                 text = "Projects",
                 style = MaterialTheme.typography.h6,
                 fontWeight = FontWeight.SemiBold,
                 //modifier = Modifier.padding(bottom = 16.dp)
             )
+            //Spacer()
+            PlusButton()
+            MinusButton()
+        }
+    }
 
-            LazyColumn {
-                items(projects) { project ->
-                    ProjectView(project = project)
+    @Composable
+    fun PlusButton() {
+        /*
+                Button(action: {
+            vc.addProject()
+            contentViewController.saveUserToJson()
+        }) {
+            Image(systemName: "plus")
+                .frame(height: 24)
+        }
+        .buttonStyle(.bordered)
+         */
+    }
+
+    @Composable
+    fun MinusButton() {
+        /*
+        Button(action: vc.onSubtractButtonTap) {
+            Image(systemName: "minus")
+            .frame(height: 24)
+        }
+            .buttonStyle(.bordered)
+         */
+    }
+
+    /*
+    private var projectColumns: [GridItem] {
+        Array(
+            repeating: GridItem(.flexible()),
+            count: UIDevice.current.userInterfaceIdiom == .phone ? 1 : 2
+        )
+    }
+     */
+
+    @Composable
+    fun ProjectContent() {
+        /*
+        ScrollView {
+            LazyVGrid(columns: projectColumns, spacing: 16) {
+                ForEach(vc.projects, id: \.id) { project in
+                    HStack {
+                        subtractProjectButton(project)
+                        let pvc = vc.projectViewController(for: project)
+                        ProjectView(pvc)
+                    }
+                    .animation(.easeInOut, value: vc.canSubtract)
                 }
             }
         }
+         */
+
+        /// The red `X` that shows up to the left of a project when the user has enabled subtracting projects
+        /*
+        private fun subtractProjectButton(_ project: YkProject) -> some View {
+            if vc.canSubtract {
+                Button(
+                    action: {
+                    vc.subtract(project: project)
+                }
+                ) {
+                Image(systemName: "x.circle.fill")
+                .foregroundColor(.pink)
+                .font(.title2)
+            }
+            }
+        }
+         */
     }
 }
-*/
