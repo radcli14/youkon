7ef77fb69a73b69e807b1060289b029b980b8bee
radcli14 <radcli14@gmail.com>
2023-11-08 16:09:57 -0500

    created a custom modifier for the edit buttons

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/EditButton.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/EditButton.kt
@@ -1,5 +1,10 @@
 package com.dcsim.youkon.android.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -8,6 +13,33 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Modifier.editButtonModifier(
+    color: Color = MaterialTheme.colors.surface,
+    alpha: Float = 0.7f,
+    width: Dp = 42.dp,
+    height: Dp = 36.dp,
+    padding: Dp = 8.dp,
+    shape: Shape = RoundedCornerShape(8.dp)
+) = composed {
+    Modifier
+        .background(
+            color = color.copy(alpha = alpha),
+            shape = shape
+        )
+        .width(width)
+        .height(height)
+        .padding(padding)
+}
+
+
 
 @Composable
 fun EditButton(onClick: () -> Unit) {
