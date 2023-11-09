f0b232fae73c687b4ecddbb9883a68da7d3114b1
radcli14 <radcli14@gmail.com>
2023-11-08 22:26:14 -0500

    many updates, getting closer to resembling the iOS version

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/ProjectView.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/ProjectView.kt
@@ -1,5 +1,8 @@
 package com.dcsim.youkon.android.views
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,75 +10,49 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.dcsim.youkon.YkMeasurement
-import com.dcsim.youkon.YkProject
 import com.dcsim.youkon.ProjectExpansionLevel
-import com.dcsim.youkon.YkSystem
+import com.dcsim.youkon.YkMeasurement
 import com.dcsim.youkon.android.viewmodels.ProjectViewModel
 
 //@Composable
 class ProjectView(
-    val vm: ProjectViewModel
+    private val vm: ProjectViewModel
 ) {
     @Composable
     fun Body() {
-        var expansion by remember { mutableStateOf(ProjectExpansionLevel.COMPACT) }
-        var measurements by remember { mutableStateOf(vm.project.measurements) }
-
-        Surface(
-            color = MaterialTheme.colors.surface.copy(alpha = 0.4f),
-            modifier = Modifier
-                .clickable {
-                    expansion = when (expansion) {
-                        ProjectExpansionLevel.COMPACT -> ProjectExpansionLevel.STATIC
-                        ProjectExpansionLevel.STATIC -> ProjectExpansionLevel.COMPACT
-                        ProjectExpansionLevel.EDITABLE -> ProjectExpansionLevel.STATIC
-                    }
-                }
-                .padding(8.dp)
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                ProjectTopRow(vm.project, expansion)
-                ProjectContent(measurements, expansion,
-                    editClick = {
-                        expansion = when (expansion) {
-                            ProjectExpansionLevel.STATIC -> ProjectExpansionLevel.EDITABLE
-                            else -> ProjectExpansionLevel.STATIC
-                        }
-                    },
-                    addClick = {
-                        println("newNewNew")
-                        vm.project.measurements.add(
-                            YkMeasurement.new()
-                        )
-                        measurements = vm.project.measurements
-                        println(measurements)
-                    }
-                )
-            }
+        if (vm.expansion.value == ProjectExpansionLevel.EDITABLE) {
+            MainStackWhenEditing()
+        } else {
+            DisclosureGroupWhenNotEditing()
         }
     }
 
     /// The list of editable measurements when the project is opened in a sheet for editing
     @Composable
     fun MainStackWhenEditing() {
-        /*
-        VStack(alignment: .leading) {
-            labelStack
-            expansionStack
+        Column(horizontalAlignment = Alignment.Start) {
+            LabelStack()
+            ExpansionStack()
         }
+        /*
         .alert(isPresented: $vc.showSubtractAlert) {
             SubtractAlert(
                 title: vc.measurementToDelete?.name ?? "",
@@ -95,47 +72,62 @@ class ProjectView(
     /// The disclosure group with static content inside, with label with name and description
     @Composable
     private fun DisclosureGroupWhenNotEditing() {
-        /*
-        DisclosureGroup(isExpanded: $vc.isExpanded) {
-            expansionStack
-        } label: {
-            labelStack
+        Surface(
+            color = MaterialTheme.colors.surface.copy(alpha = 0.4f),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .clickable {
+                    vm.expansion.value = when (vm.expansion.value) {
+                        ProjectExpansionLevel.COMPACT -> ProjectExpansionLevel.STATIC
+                        ProjectExpansionLevel.STATIC -> ProjectExpansionLevel.COMPACT
+                        ProjectExpansionLevel.EDITABLE -> ProjectExpansionLevel.STATIC
+                    }
+                }
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                LabelStack()
+                ExpansionStack()
+            }
         }
-                .padding()
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(8)
-         */
     }
 
     /// The name and description shown at the top of the project
     @Composable
     private fun LabelStack() {
-        /*
-        HStack {
-            projectImage
-            VStack(alignment: .leading) {
-            nameField
-            descriptionField
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                NameField()
+                DescriptionField()
+            }
+            CloseIcon(vm.expansion.value)
         }
-            .foregroundStyle(.foreground)
-        }
-         */
     }
 
     /// The content that is displayed when the `DisclosureGroup` is expanded
     @Composable
     private fun ExpansionStack() {
-        /*
-        VStack {
+        AnimatedVisibility(
+            visible =vm.expansion.value != ProjectExpansionLevel.COMPACT,
+            enter = expandVertically(),
+            exit = shrinkVertically()
+        ) {
             Divider()
-            systemPicker
-            HStack(alignment: .top) {
-            expansionView
-            Spacer()
-            expansionMenu
+            Column {
+                Divider()
+                SystemPicker()
+                Row(verticalAlignment = Alignment.Top) {
+                    ExpansionView()
+                    Spacer(Modifier.weight(1f))
+                    ExpansionMenu()
+                }
+            }
         }
-        }
-         */
     }
 
     @Composable
@@ -171,49 +163,58 @@ class ProjectView(
     /// The title of the project, which is the `.name` field in the `YkProject`
     @Composable
     private fun NameField() {
-        /*
-        switch (vc.expansion) {
-            case .editable:
-            TextField("Name", text: $vc.editedName)
-            .font(.title3)
-            .fontWeight(.bold)
-            .onChange(of: vc.editedName) { name in
-                vc.project.name = name
+        when (vm.expansion.value) {
+            ProjectExpansionLevel.EDITABLE -> {
+                /*
+                TextField("Name", text: $vc.editedName)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .onChange(of: vc.editedName) { name in
+                        vc.project.name = name
+                }
+                 */
+            }
+            else -> {
+                Text(
+                    text = vm.project.name,
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
         }
-            default:
-            Text(vc.editedName)
-                .font(.headline)
-            .fontWeight(.bold)
-        }
-         */
     }
 
     /// The subtitle of the project, which is the `.about` field in the `YkProject`
     @Composable
     private fun DescriptionField() {
-        /*
-        switch (vc.expansion) {
-            case .editable:
-            TextField("Description", text: $vc.editedDescription)
-            .onChange(of: vc.editedDescription) { description in
-                vc.project.about = description
+        when (vm.expansion.value) {
+            ProjectExpansionLevel.EDITABLE -> {
+                /*
+                TextField("Description", text: $vc.editedDescription)
+                    .onChange(of: vc.editedDescription) { description in
+                        vc.project.about = description
+                }
+                 */
+            }
+            else -> {
+                Text(
+                    text = vm.editedDescription.value,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+            }
         }
-            .font(.body)
-            default:
-            Text(vc.editedDescription)
-                .font(.caption)
-        }
-         */
     }
 
     /// When the `DisclosureGroup` is expanded, this will be inside, and will either contain the editable content when opened in a `.sheet`, or static text
     @Composable
     private fun ExpansionView() {
-        //expansionMeasurements
+        ExpansionMeasurements()
     }
 
     @Composable
     private fun ExpansionMeasurements() {
+        ExpansionMeasurementsList()
         /*
         ScrollView {
             VStack(alignment: .leading) {
@@ -231,154 +232,112 @@ class ProjectView(
     /// A `ForEach` corresponding to each of the measurements, in either editable or static form
     @Composable
     private fun ExpansionMeasurementsList() {
-        /*
-        ForEach(vc.measurements, id: \.id) { measurement in
-                switch (vc.expansion) {
-                    case .editable: editableMeasurement(measurement)
-                    default: staticMeasurement(measurement)
+        Column {
+            vm.measurements.value.forEach { measurement ->
+                when (vm.expansion.value) {
+                    ProjectExpansionLevel.EDITABLE -> EditableMeasurement(measurement)
+                    else -> StaticMeasurement(measurement)
                 }
+            }
         }
-        if vc.measurements.isEmpty {
-            Text("Add New Measurements")
+        if (vm.measurements.value.isEmpty()) {
+            Text("Add New Measuremenbts")
         }
-
-         */
     }
 
     /// In the `expansionMeasurementList`, this is a single measurement that is editable
     @Composable
     private fun EditableMeasurement(measurement: YkMeasurement) {
-        /*
-        HStack {
-            subtractMeasurementButton(measurement)
-            MeasurementView(measurement: measurement)
+        Row {
+            SubtractMeasurementButton(measurement)
+            MeasurementView(measurement)
         }
-            .animation(.easeInOut, value: vc.canSubtract)
-
-         */
     }
 
     /// In the `expansionMeasurementList`, this is a single measurement that is not editable
     @Composable
-    private fun staticMeasurement(measurement: YkMeasurement) {
-        /*
-        VStack(alignment: .leading) {
+    private fun StaticMeasurement(measurement: YkMeasurement) {
+        Column(horizontalAlignment = Alignment.Start) {
             Divider()
-            Text(measurement.name)
-                .font(.caption)
-            .fontWeight(.bold)
-            Text(measurement.about)
-                .font(.caption2)
-            .foregroundStyle(.secondary)
-            Text(measurement.convertToSystem(targetSystem: vc.convertToSystem).valueString)
+            Text(measurement.name,
+                style = MaterialTheme.typography.body2,
+                fontWeight = FontWeight.Bold
+            )
+            Text(measurement.about,
+                style = MaterialTheme.typography.caption
+            )
+            Text(measurement.convertToSystem(vm.convertToSystem.value).valueString)
         }
-            .multilineTextAlignment(.leading)
-
-         */
     }
 
     /// If editable, this will display the `expansionPlusMinusStack`
     @Composable
     private fun ExpansionMenu() {
-        /*
-        if vc.expansion == .editable {
-            expansionPlusMinusStack
+        if (vm.expansion.value == ProjectExpansionLevel.EDITABLE) {
+            ExpansionPlusMinusStack()
         }
-         */
     }
 
     /// The plus and minus buttons on the right hand side when editing, to create or delete measurements
     @Composable
     private fun ExpansionPlusMinusStack() {
-        /*
-        VStack {
-            Button(action: vc.addMeasurement) {
-            Image(systemName: "plus")
-            .frame(height: 24)
+        Column {
+            PlusButton()
+            MinusButton()
         }
-            Button(action: vc.subtractMeasurement) {
-            Image(systemName: "minus")
-            .frame(height: 24)
+    }
+
+    @Composable
+    fun PlusButton() {
+        IconButton(
+            onClick = {
+                vm.addMeasurement()
+                //mainViewModel.saveUserToJson()
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add a new measurement",
+                modifier = Modifier.editButtonModifier(),
+                tint = MaterialTheme.colors.primary
+            )
         }
+    }
+
+    @Composable
+    fun MinusButton() {
+        IconButton(
+            onClick = { vm.subtractMeasurement() }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Remove,
+                contentDescription = "Allow deleting projects",
+                modifier = Modifier.editButtonModifier(),
+                tint = MaterialTheme.colors.primary
+            )
         }
-            .buttonStyle(.bordered)
-        .foregroundColor(.indigo)
-         */
     }
 
     /// The red `X` that shows up to the left of a measurement when the user has enabled subtracting measurements
     @Composable
-    private fun subtractMeasurementButton(measurement: YkMeasurement) {
-        /*
-        if vc.canSubtract {
-            Button(
-                action: {
-                vc.subtract(measurement: measurement)
-            }
+    private fun SubtractMeasurementButton(measurement: YkMeasurement) {
+        AnimatedVisibility(vm.canSubtract.value) {
+            IconButton(
+                onClick = { vm.subtract(measurement) }
             ) {
-            Image(systemName: "x.circle.fill")
-            .foregroundColor(.pink)
-            .font(.title2)
-        }
-        }
-
-         */
-    }
-}
-
-
-/// Top row with the name and description of the project, and button to expand/close
-@Composable
-fun ProjectTopRow(project: YkProject, expansion: ProjectExpansionLevel) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Column {
-            NameText(project.name)
-            DescriptionText(project.about)
-        }
-        CloseIcon(expansion)
-    }
-}
-
-
-/// Content with either static or editable measurements
-@Composable
-fun ProjectContent(
-    measurements: List<YkMeasurement>,
-    expansion: ProjectExpansionLevel,
-    editClick: () -> Unit,
-    addClick: () -> Unit
-) {
-    Row {
-        Column {
-            if (expansion == ProjectExpansionLevel.EDITABLE) {
-                // Editable fields for each measurement and unit selection
-                measurements.forEach { measurement ->
-                    MeasurementView(measurement = measurement)
-                }
-            } else if (expansion == ProjectExpansionLevel.STATIC) {
-                // Displays of the measurement after conversion to a consistent set of units
-                measurements.forEach { measurement ->
-                    Text(measurement.nameAndValueInSystem(YkSystem.SI))
-                }
-            }
-        }
-
-        // The edit button is visible when the project has been expanded beyond its compact level.
-        // When it is tapped, the value, units, name, and description fields can be edited.
-        // Tap again to collapse to the static level, in which you see but don't modify measurements.
-        if (expansion != ProjectExpansionLevel.COMPACT) {
-            Spacer(Modifier.weight(1f))
-            Card {
-                Column {
-                    EditButton { editClick() }
-                    if (expansion == ProjectExpansionLevel.EDITABLE) {
-                        PlusButton { addClick() }
-                        MinusButton { }
-                    }
-                }
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Delete ${measurement.name} measurement",
+                    modifier = Modifier.editButtonModifier(
+                        color = MaterialTheme.colors.error,
+                        alpha = 1f,
+                        width = 24.dp,
+                        height = 24.dp,
+                        padding = 4.dp,
+                        shape = CircleShape
+                    ),
+                    tint = MaterialTheme.colors.onPrimary
+                )
             }
         }
     }
