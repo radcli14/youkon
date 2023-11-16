05c4f9c1c285de68848b47ebd7f12b6299831fac
radcli14 <radcli14@gmail.com>
2023-11-15 21:27:34 -0500

    name, description, value, and unit changes in a measurement propagate up to the project interface

--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/viewmodels/MeasurementViewModel.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/viewmodels/MeasurementViewModel.kt
@@ -2,55 +2,49 @@ package com.dcsim.youkon.android.viewmodels
 
 import android.util.Log
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.dcsim.youkon.YkMeasurement
 import com.dcsim.youkon.YkUnit
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
 
 class MeasurementViewModel(initialMeasurement: YkMeasurement): ViewModel() {
     val measurement = MutableStateFlow(initialMeasurement)
-    var equivalentUnits by mutableStateOf(initialMeasurement.unit.equivalentUnits())
 
     private val tag = "MeasurementViewModel"
 
-    private val measurementName get() = measurement.value.name
-    private val measurementDescription get() = measurement.value.name
-    private val value get() = measurement.value.value
-    private val unit get() = measurement.value.unit
+    var measurementName by mutableStateOf(initialMeasurement.name)
+    var measurementDescription by mutableStateOf(initialMeasurement.about)
+    var value by mutableDoubleStateOf(initialMeasurement.value)
+    var unit by mutableStateOf(initialMeasurement.unit)
 
     fun updateName(newName: String) {
         Log.d(tag, "value updated from $measurementName to $newName")
-        measurement.update { currentMeasurement ->
-            currentMeasurement.copy(name = newName)
-        }
+        measurement.value.name = newName
+        measurementName = newName
     }
 
     fun updateDescription(newDescription: String) {
         Log.d(tag, "value updated from $measurementDescription to $newDescription")
-        measurement.update { currentMeasurement ->
-            currentMeasurement.copy(about = newDescription)
-        }
+        measurement.value.about = newDescription
+        measurementDescription = newDescription
     }
 
     /// When the user modifies the value in the `MeasurementTextField` update the `value`
     fun updateValue(newValue: Double) {
         Log.d(tag, "value updated from $value to $newValue")
-        measurement.update { currentMeasurement ->
-            currentMeasurement.copy(value = newValue)
-        }
+        measurement.value.value = newValue
+        value = newValue
     }
 
     /// When the user modifies the `From` dropdown, update the `measurement.unit`
     fun updateUnit(newUnit: YkUnit?) {
         newUnit?.let {
             Log.d(tag, "unit updated from $unit to $it")
-            measurement.update { currentMeasurement ->
-                currentMeasurement.copy(unit = it)
-            }
-            equivalentUnits = it.equivalentUnits()
+            measurement.value.unit = it
+            unit = it
         }
     }
 }
--- youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/MeasurementView.kt
+++ youkon/YouKon/src/main/java/com/dcsim/youkon/android/views/MeasurementView.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -28,9 +27,8 @@ class MeasurementView(measurement: YkMeasurement) {
     /// Editable field for the name of the `YkMeasurement`
     @Composable
     private fun NameField() {
-        val measurement = vm.measurement.collectAsState()
         BasicTextFieldWithHint(
-            value = measurement.value.name,
+            value = vm.measurementName,
             hint = "name",
             onValueChange = { vm.updateName(it) },
             textStyle = MaterialTheme.typography.subtitle1.copy(
@@ -42,9 +40,9 @@ class MeasurementView(measurement: YkMeasurement) {
     /// Editable field for the `about` string of the `YkMeasurement
     @Composable
     private fun DescriptionField() {
-        val measurement = vm.measurement.collectAsState()
+        //val measurement = vm.measurement.collectAsState()
         BasicTextFieldWithHint(
-            value = measurement.value.about,
+            value = vm.measurementDescription,
             hint = "description",
             onValueChange = { vm.updateDescription(it) },
             textStyle = MaterialTheme.typography.body1.copy(
@@ -56,19 +54,18 @@ class MeasurementView(measurement: YkMeasurement) {
     /// Numeric field on the left to modify `value`, and dropdown on the right to modify `unit` of the `YkMeasurement`
     @Composable
     private fun ValueAndUnitStack() {
-        val measurement = vm.measurement.collectAsState()
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             MeasurementTextField(
-                initialText = measurement.value.value.toString(),
+                initialText = vm.value.toString(),
                 modifier = Modifier.weight(1f),
                 updateMeasurement = { vm.updateValue(it) }
             )
             UnitDropdown(
-                unit = measurement.value.unit,
-                availableUnits = measurement.value.unit.allUnits,
+                unit = vm.unit,
+                availableUnits = vm.unit.allUnits,
                 modifier = Modifier.weight(1f),
                 onClick = { vm.updateUnit(it) }
             ).Body()
