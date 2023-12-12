87145fc9de174a8eff6d32da79cdf3fde202d267
radcli14 <radcli14@gmail.com>
2023-12-11 22:52:39 -0500

    switched to using the Material 3 bottom sheet scaffold

--- youkon/YouKon/src/main/java/com/dcengineer/youkon/views/MainView.kt
+++ youkon/YouKon/src/main/java/com/dcengineer/youkon/views/MainView.kt
@@ -1,28 +1,33 @@
 package com.dcengineer.youkon.views
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dcengineer.youkon.ProjectExpansionLevel
@@ -30,6 +35,7 @@ import com.dcengineer.youkon.YoukonTheme
 import com.dcengineer.youkon.viewmodels.MainViewModel
 import com.dcengineer.youkon.viewmodels.ProjectsCardViewModel
 import com.dcengineer.youkon.viewmodels.QuickConvertCardViewModel
+import kotlinx.coroutines.launch
 
 
 class MainView(
@@ -49,36 +55,53 @@ class MainView(
         }
     }
 
-    @OptIn(ExperimentalMaterialApi::class)
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun BottomSheetLayout() {
-        val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+        // Set up the scaffold state such that it clears the project being edited from the
+        // view model if the user drags the sheet downward. Will do the same if the user taps
+        // the floating action button in the lower right.
+        val scaffoldState = rememberBottomSheetScaffoldState(
+            bottomSheetState = rememberModalBottomSheetState(
+                confirmValueChange = {
+                    if (it == SheetValue.Hidden) {
+                        mainViewModel.stopEditing()
+                    }
+                    false
+                }
+            )
+        )
+
+        // React to changes in mainViewModel.isEditingProject by expanding or collapsing
+        val scope = rememberCoroutineScope()
         val isBottomSheetExpanded by mainViewModel.isEditingProject.observeAsState()
         LaunchedEffect(isBottomSheetExpanded) {
             if (isBottomSheetExpanded == true) {
-                sheetState.show()
+                scope.launch { scaffoldState.bottomSheetState.expand() }
             } else {
-                sheetState.hide()
+                scope.launch { scaffoldState.bottomSheetState.hide() }
             }
         }
 
-        ModalBottomSheetLayout(
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 360.dp,
             sheetContent = {
                 ProjectEditingSheet()
-                LaunchedEffect(sheetState.currentValue) {
-                    if (sheetState.currentValue == ModalBottomSheetValue.Hidden) {
-                        mainViewModel.stopEditing()
-                    }
-                }
             },
-            sheetBackgroundColor = MaterialTheme.colorScheme.surface,
-            sheetState = sheetState,
-            sheetShape = MaterialTheme.shapes.large.copy(
-                bottomStart = CornerSize(0.dp),
-                bottomEnd = CornerSize(0.dp)
-            )
+            modifier = Modifier.tapOutside()
         ) {
             MainContentStack()
+        }
+    }
+
+    /// Modifier used to close the bottom sheet when tapping outside of it
+    @SuppressLint("UnnecessaryComposedModifier")
+    private fun Modifier.tapOutside() = composed {
+        Modifier.pointerInput(Unit) {
+            detectTapGestures(
+                onTap = { mainViewModel.stopEditing() }
+            )
         }
     }
 
