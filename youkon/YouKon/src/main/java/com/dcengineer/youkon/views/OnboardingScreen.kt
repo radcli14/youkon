b08bca994523da476b882cf59a36873a40d24346
radcli14 <radcli14@gmail.com>
2023-12-19 12:50:17 -0500

    added a second line to the onboarding text

--- youkon/YouKon/src/main/java/com/dcengineer/youkon/views/OnboardingScreen.kt
+++ youkon/YouKon/src/main/java/com/dcengineer/youkon/views/OnboardingScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -31,36 +32,44 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 class OnboardingScreen {
-    private val screens = arrayOf(
-        "Welcome",
-        "Quick Convert Card",
-        "Projects Card",
-        "Project View",
-        "Editable Project"
+    private val helps = arrayOf(
+        "Welcome" to
+            "Thank you for trying the unit converter app designed for engineers",
+        "Quick Convert Card" to
+            "1. Tap the `From` button in the upper left to select from a list of all available units in the app\n" +
+            "2. Tap the `To` button in the upper right to select from a list of units that may be converted given the `From` unit\n" +
+            "3. Enter a number in the lower left for a value in the `From` unit\n" +
+            "4. The converted value and unit are displayed in the lower right",
+        "Projects Card" to
+            "Here you may store all of your projects, each with multiple measurements, all converted to a consistent system of units\n" +
+            "1. Tap the `Plus` (+) button to add a new project\n" +
+            "2. Tap the `Minus` (-) button to enable subtracting a project, then tap the `X` button alongside to delete that project",
+        "Project View" to
+            "1. Tap once on a project to expand it to show its list of measurements, and select a system of units\n" +
+            "2. Tap on the list of measurements to open the editing screen for that project",
+        "Editable Project" to
+            "The bottom sheet expands to show a menu where you may modify data in this project\n" +
+            "1. Name and description fields at the top are used to edit the header and labels for this project\n" +
+            "2. `Plus` (+) and `Minus` (-) are used to add and subtract measurements\n" +
+            "3. Each measurement has its own name and description field\n" +
+            "4. Number value and a unit may be selected for each measurement"
     )
 
     @Composable
     fun Body() {
         var currentPage by remember { mutableIntStateOf(0) }
-        Column {
+        Column(
+            modifier = Modifier.background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = MaterialTheme.shapes.large
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Box(
-                modifier = Modifier
-                    .fillMaxHeight(0.9f)
-                    .background(
-                        color = MaterialTheme.colorScheme.surface,
-                        shape = MaterialTheme.shapes.large
-                    )
+                modifier = Modifier .fillMaxHeight(0.9f)
             ) {
-                Text(screens[currentPage],
-                    modifier = Modifier
-                        .align(Alignment.TopStart)
-                        .padding(16.dp),
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-
+                OnboardText(currentPage, Modifier.align(Alignment.TopStart))
                 ScaledMainView()
-
                 NavButton(Modifier.align(Alignment.BottomEnd)) {
                     currentPage = if (currentPage < 4) currentPage + 1 else 0
                 }
@@ -77,13 +86,29 @@ class OnboardingScreen {
         }
     }
 
+    /// Shows two lines of text at the top, with the title of the current screen, and some helpful text
+    @Composable
+    fun OnboardText(currentPage: Int, modifier: Modifier = Modifier) {
+        Column(modifier = modifier.padding(16.dp)) {
+            Text(helps[currentPage].first,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(helps[currentPage].second,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+
     /// A row of tabs at the bottom of the screen
     @Composable
     fun Tabs(currentPage: Int, onTabSelected: (Int) -> Unit) {
         TabRow(
+            modifier = Modifier.fillMaxWidth(0.7f),
             selectedTabIndex = currentPage,
         ) {
-            screens.forEachIndexed { index, _ ->
+            helps.forEachIndexed { index, _ ->
                 Tab(selected = index == currentPage, onClick = {
                     onTabSelected(index)
                 }, modifier = Modifier.padding(16.dp)) {
