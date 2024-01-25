07cecd1ee83dc123e1b6086cc568e23cedccce9f
radcli14 <radcli14@gmail.com>
2024-01-25 16:54:12 +0100

    added template version of the compose multiplatform files

---
+++ YouKonCMP/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+}
+
+task("sharedCopy") {
+    copy {
+        from("../youkon/youkon/shared/src/commonMain/kotlin/com/dcengineer/youkon")
+        into("./composeApp/commonMain/kotlin/model")
+    }
+}
