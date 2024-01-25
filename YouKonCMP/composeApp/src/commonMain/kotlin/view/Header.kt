f98f20568c0335d0b25b8837791f22882ababb5c
radcli14 <radcli14@gmail.com>
2024-01-25 22:33:35 +0100

    temporarily removing resource dependencies

--- YouKonCMP/composeApp/src/commonMain/kotlin/view/Header.kt
+++ YouKonCMP/composeApp/src/commonMain/kotlin/view/Header.kt
@@ -10,28 +10,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
+//import androidx.compose.ui.text.font.Font
+//import androidx.compose.ui.text.font.FontFamily
+//import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.dcengineer.youkon.R
+//import com.dcengineer.youkon.R
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
+/*
 val philosopherFamily = FontFamily(
     Font(R.font.philosopher_regular, FontWeight.Normal),
     Font(R.font.philosopher_italic, FontWeight.Normal, FontStyle.Italic),
     Font(R.font.philosopher_bold, FontWeight.Bold),
     Font(R.font.philosopher_bolditalic, FontWeight.Bold, FontStyle.Italic)
 )
+*/
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun Header() {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
-            painter = painterResource(id = R.drawable.icon_clearbackground),
+            painter = painterResource("icon_clearbackground.png"), //(id = R.drawable.icon_clearbackground),
             contentDescription = "App icon",
             modifier = Modifier.width(90.dp),
             colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
@@ -40,7 +44,7 @@ fun Header() {
             fontWeight = FontWeight.Bold,
             style = MaterialTheme.typography.displayLarge,
             color = MaterialTheme.colorScheme.onSurface,
-            fontFamily = philosopherFamily
+            //fontFamily = philosopherFamily
         )
         Spacer(modifier = Modifier.width(90.dp))
     }
