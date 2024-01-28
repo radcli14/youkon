e14d99fb354bb3920a4a9deb13b4a728f924f76a
Eliott Radcliffe <Eliott.Radcliffe@dc-engineer.com>
2024-01-28 19:25:28 +0100

    created singletons of the view models

--- YouKonCMP/composeApp/src/iosMain/kotlin/MainViewController.kt
+++ YouKonCMP/composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,13 +1,13 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import viewmodel.MainViewModel
 import viewmodel.OnboardingScreenViewModel
-import viewmodel.ProjectsCardViewModel
 import viewmodel.QuickConvertCardViewModel
 
+private val mainViewModel = MainViewModel()
+private val quickConvertCardViewModel = QuickConvertCardViewModel()
+private val onboardingScreenViewModel = OnboardingScreenViewModel()
+
 fun MainViewController() = ComposeUIViewController {
-    val mainViewModel = MainViewModel()
-    val quickConvertCardViewModel = QuickConvertCardViewModel()
-    val onboardingScreenViewModel = OnboardingScreenViewModel()
     App(
         mainViewModel,
         quickConvertCardViewModel,
