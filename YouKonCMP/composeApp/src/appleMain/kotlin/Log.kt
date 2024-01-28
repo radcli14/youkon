a3be014b3cabedd079c7f013b768be2ab08f1029
Eliott Radcliffe <Eliott.Radcliffe@dc-engineer.com>
2024-01-28 10:19:10 +0100

    use println instead of print

--- YouKonCMP/composeApp/src/appleMain/kotlin/Log.kt
+++ YouKonCMP/composeApp/src/appleMain/kotlin/Log.kt
@@ -1,7 +1,7 @@
 actual class Log {
     actual companion object {
         actual fun d(tag: String, message: String) {
-            print("debug $tag: $message")
+            println("debug $tag: $message")
         }
     }
 }
