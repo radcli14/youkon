951cf5cda174f1a2aa67aea31aaacc3f971c0317
Eliott Radcliffe <Eliott.Radcliffe@dc-engineer.com>
2024-01-29 18:52:14 +0100

    deleted unnecessary comment

--- YouKonCMP/composeApp/src/commonMain/kotlin/view/MeasurementTextField.kt
+++ YouKonCMP/composeApp/src/commonMain/kotlin/view/MeasurementTextField.kt
@@ -43,7 +43,7 @@ fun MeasurementTextField(
         ) {
             BasicTextField(
                 value = text,
-                modifier = Modifier.weight(1f), //.padding(6.dp)
+                modifier = Modifier.weight(1f),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 singleLine = true,
                 onValueChange = { newText ->
