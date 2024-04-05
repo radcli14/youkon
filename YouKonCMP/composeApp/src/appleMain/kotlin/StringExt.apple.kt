55b06a6a79d6fdc9e35b0e7ed6febecdc292eabe
radcli14 <radcli14@gmail.com>
2024-04-05 19:47:14 +0200

    should be working email and password checks

--- YouKonCMP/composeApp/src/appleMain/kotlin/StringExt.apple.kt
+++ YouKonCMP/composeApp/src/appleMain/kotlin/StringExt.apple.kt
@@ -1,7 +1,19 @@
+import platform.Foundation.NSRegularExpression
+import platform.Foundation.NSRegularExpressionCaseInsensitive
+import platform.Foundation.NSString
+//import platform.Foundation.NSStringMatchingAnchored
+import platform.Foundation.NSCharacterSet
+
+private const val MIN_PASS_LENGTH = 6
+private const val EMAIL_PATTERN = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+private const val PASS_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$"
+
 actual fun String.isValidEmail(): Boolean {
-    TODO("Not yet implemented")
+    return this.isNotBlank() && Regex(EMAIL_PATTERN).matches(this)
 }
 
 actual fun String.isValidPassword(): Boolean {
-    TODO("Not yet implemented")
+    return this.isNotBlank() &&
+            this.length >= MIN_PASS_LENGTH &&
+            Regex(PASS_PATTERN).matches(this)
 }
--- YouKonCMP/composeApp/src/iosMain/kotlin/Platform.ios.kt
+++ YouKonCMP/composeApp/src/iosMain/kotlin/Platform.ios.kt
@@ -5,9 +5,6 @@ class IOSPlatform: Platform {
 }
 
 actual fun getPlatform(): Platform {
-    println("getPlatform()")
     val name = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    println(name)
-    println("iOS" in name)
     return IOSPlatform()
 }
