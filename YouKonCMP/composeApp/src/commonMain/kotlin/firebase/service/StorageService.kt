7f42f18f3f59f34b4960d17e32755d075ab57fb9
radcli14 <radcli14@gmail.com>
2024-04-08 17:57:46 +0200

    cleaned up the implementation of projects

--- YouKonCMP/composeApp/src/commonMain/kotlin/firebase/service/StorageService.kt
+++ YouKonCMP/composeApp/src/commonMain/kotlin/firebase/service/StorageService.kt
@@ -2,8 +2,11 @@ package firebase.service
 
 import AccountService
 import dev.gitlive.firebase.firestore.FirebaseFirestore
+import dev.gitlive.firebase.firestore.where
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import model.YkProject
 import trace
 
@@ -20,45 +23,41 @@ class StorageServiceImpl(
     private val firestore: FirebaseFirestore,
     private val auth: AccountService
 ) : StorageService {
-    /*
+
     @OptIn(ExperimentalCoroutinesApi::class)
     override val projects: Flow<List<YkProject>>
         get() =
             auth.currentUser.flatMapLatest { user ->
-                firestore.collection(PROJECT_COLLECTION) //.whereEqualTo(USER_ID_FIELD, user.id).dataObjects()
+                firestore.collection(PROJECT_COLLECTION)
+                    .where { USER_ID_FIELD equalTo user.id }
+                    .snapshots
+                    .map { snapshot ->
+                        snapshot.documents.map { document ->
+                            document.data(YkProject.serializer())
+                        }
+                    }
             }
 
     override suspend fun getProject(projectId: String): YkProject? =
-        firestore.collection(PROJECT_COLLECTION).document(projectId).get() //.await().toObject()
-*/
-    override val projects: Flow<List<YkProject>>
-        get() = callbackFlow {
-            //TODO("Not yet implemented")
-        }
-
-    override suspend fun getProject(projectId: String): YkProject? {
-        //TODO("Not yet implemented")
-        return null
-    }
+        firestore.collection(PROJECT_COLLECTION).document(projectId).get().data(YkProject.serializer())
 
     override suspend fun save(project: YkProject): String =
         trace(SAVE_PROJECT_TRACE) {
-            val projectWithUserId = project //.copy(userId = auth.currentUserId)
-            //firestore.collection(PROJECT_COLLECTION).add(projectWithUserId).id //.await().id
-            "TODO: Not Yet Implemented"
+            firestore.collection(PROJECT_COLLECTION).add(project).id
         }
 
     override suspend fun update(project: YkProject): Unit =
         trace(UPDATE_PROJECT_TRACE) {
-            //firestore.collection(PROJECT_COLLECTION).document(project.id).set(project) //.await()
+            firestore.collection(PROJECT_COLLECTION).document(project.id).set(project)
         }
 
     override suspend fun delete(taskId: String) {
-        firestore.collection(PROJECT_COLLECTION).document(taskId).delete() //.await()
+        firestore.collection(PROJECT_COLLECTION).document(taskId).delete()
     }
 
     companion object {
         private const val USER_ID_FIELD = "userId"
+        private const val USER_DATA_COLLECTION = "userData"
         private const val PROJECT_COLLECTION = "projects"
         private const val SAVE_PROJECT_TRACE = "saveProject"
         private const val UPDATE_PROJECT_TRACE = "updateProject"
