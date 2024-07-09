f6dc7b7354869c20acce70c9b70be3db21e5f8ed
radcli14 <radcli14@gmail.com>
2024-07-09 16:22:48 +0200

    added a preview of the unit dropdown

---
+++ YouKonCMP/composeApp/src/androidMain/kotlin/com/dcengineer/youkon/PreviewUnitDropdown.kt
@@ -0,0 +1,12 @@
+package com.dcengineer.youkon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import model.YkUnit
+import view.UnitDropdown
+
+@Preview
+@Composable
+fun UnitDropdownPreview() {
+    UnitDropdown(YkUnit.METERS, YkUnit.METERS.allUnits).Body()
+}
