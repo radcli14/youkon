2fcd573cfdab4bdabf041bf49a12e900f5cf35ac
radcli14 <radcli14@gmail.com>
2025-06-11 16:52:20 +0200

    deleted platform dependent padding in the background box

--- YouKonCMP/composeApp/src/commonMain/kotlin/view/BackgroundBox.kt
+++ YouKonCMP/composeApp/src/commonMain/kotlin/view/BackgroundBox.kt
@@ -4,25 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import getPlatform
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import youkon.composeapp.generated.resources.Res
 import youkon.composeapp.generated.resources.background
 
 /// Holds the background image, content overlaid on top of it
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun BackgroundBox(content: @Composable () -> Unit) {
-    val isIphone = "iOS" in getPlatform().name
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.TopCenter
@@ -36,15 +30,7 @@ fun BackgroundBox(content: @Composable () -> Unit) {
                 colorMatrix(if (isSystemInDarkTheme()) -80f else 80f)
             )
         )
-        Box(
-            modifier = Modifier
-                .padding(
-                    top = if (isIphone) 36.dp else 24.dp,
-                    bottom = if (isIphone) 12.dp else 0.dp
-                )
-        ) {
-            content()
-        }
+        content()
     }
 }
 
